c78b4c9788baf89f4dbc41e637ba595968fdce80
Kirrrkaz <74609707+Kirrrkaz@users.noreply.github.com>
2021-08-19 21:32:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+# -*- coding: utf8 -*-
+'''
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+'''
 from aiogram import Bot, Dispatcher, executor, types
 import requests
 import logging
@@ -7,10 +13,28 @@ from bs4 import BeautifulSoup as bs
 url = "https://www.securitylab.ru/news/"
 request = requests.get(url)
 soup = bs(request.text, "html.parser")
-content = soup.find_all("div", class_="article-card-details")
+article_cards = soup.find_all("a", class_="article-card")
+'''
+for article in article_cards:
+    title = article.find_all("h2", class_="article-card-title")
+    
+    for title in title:
+        print(title.text)
+'''
 
-for content in content:
-    print(content.text)
+for article in article_cards:
+    article_title = article.find("h2", class_="article-card-title")
+    article_title = str(article_title).split('<h2 class="article-card-title">')[1].split('  </h2>')[0]
+    article_desc = article.find("p")
+    article_url = f'https://www.securitylab.ru{article.get("href")}'
+    #article_date_time = article.find("time")
+
+
+    print(f"{article_title} \n {article_url}")
+
+#for title in title:
+#print(title.text)
+
 
 
 API_TOKEN = '...'
@@ -23,12 +47,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply('Привет, пришли мне ссылку на ресурс')
+    await message.reply('Привет,напиши /news и я вышлю инфу')
 
 @dp.message_handler()
-async def message(message: 'https://www.securitylab.ru/news/' ):
-    await message.answer(content.text)
-
+async def message(message: '/news' ):
+    await message.answer(title.text)
 
 
 
